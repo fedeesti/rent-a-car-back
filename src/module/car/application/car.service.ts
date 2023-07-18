432a531d06523d7a97e5d9c436d949bbd4951d5d
github_fedeesti <fede.estigarribia@hotmail.com>
2023-07-18 16:42:00 -0300

    Correct conflicts when merging pull requests

--- src/module/car/application/car.service.ts
+++ src/module/car/application/car.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { DeleteResult, Repository } from 'typeorm';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
 import { Car } from '../domain/car.entity';
 import { CreateCarDto } from '../interface/create-car.dto';
 
 @Injectable()
 export class CarService {
   constructor(@InjectRepository(Car) private carRepository: Repository<Car>) {}
+
+  findAll(): Promise<Car[]> {
+    return this.carRepository.find();
+  }
 
   findById(id: number): Promise<Car> {
     return this.carRepository.findOne({
@@ -16,14 +20,16 @@ export class CarService {
     });
   }
 
-  delete(id: number): Promise<DeleteResult> {
-    return this.carRepository.delete({ id });
-}
-  findAll(): Promise<Car[]> {
-    return this.carRepository.find();
+  create(car: CreateCarDto): Promise<Car> {
+    const newCar = this.carRepository.create(car);
+    return this.carRepository.save(newCar);
   }
 
   update(id: number, fieldsToUpdate: Partial<CreateCarDto>): Promise<UpdateResult> {
     return this.carRepository.update({ id }, fieldsToUpdate);
+  }
+
+  delete(id: number): Promise<DeleteResult> {
+    return this.carRepository.delete({ id });
   }
 }
--- src/module/car/interface/car.controller.ts
+++ src/module/car/interface/car.controller.ts
@@ -1,13 +1,27 @@
-
-import { Body, Controller, Param, Post, Patch } from '@nestjs/common';
+import { Body, Controller, Param, Post, Patch, Get, Delete } from '@nestjs/common';
 import { CarService } from '../application/car.service';
 import { CreateCarDto } from './create-car.dto';
 import { Car } from '../domain/car.entity';
-import { UpdateResult } from 'typeorm';
+import { DeleteResult, UpdateResult } from 'typeorm';
 
 @Controller('cars')
 export class CarController {
   constructor(private readonly service: CarService) {}
+
+  @Get()
+  getCars(): Promise<Car[]> {
+    return this.service.findAll();
+  }
+
+  @Get(':id')
+  getUser(@Param('id') id: string): Promise<Car> {
+    return this.service.findById(Number(id));
+  }
+
+  @Post()
+  save(@Body() newCar: CreateCarDto): Promise<Car> {
+    return this.service.create(newCar);
+  }
 
   @Patch(':id')
   update(
@@ -15,11 +29,10 @@ export class CarController {
     @Body() fieldsToUpdate: Partial<CreateCarDto>
   ): Promise<UpdateResult> {
     return this.service.update(Number(id), fieldsToUpdate);
-
   }
 
-  @Post()
-  save(@Body() newCar: CreateCarDto): Promise<Car> {
-    return this.service.create(newCar);
+  @Delete()
+  delete(@Param('id') id: string): Promise<DeleteResult> {
+    return this.service.delete(Number(id));
   }
 }
